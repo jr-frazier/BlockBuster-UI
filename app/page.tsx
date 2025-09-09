f1408d394685a84c86384a19123be09824550967
JR Frazier <justinfrazier@jrs-mbp-2.mynetworksettings.com>
2025-09-08 20:44:43 -0400

    added movie image files

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
     return (
         <div id="main">
             <h1>Hello World</h1>
-
+            <Image src="/movies/top_gun.jpg" width={100} height={100}
+                   alt="Picture of the author"/>
         </div>
     );
 }
